d074a3d7b5999b56bee6ce72f481fd21361cf195
damieroliverroot <87322409+damieroliverroot@users.noreply.github.com>
2022-07-12 17:09:57 +0700

    Update Test.java
    Test Daffa

--- Test.java
+++ Test.java
@@ -2,11 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HELLO !!!");
         System.out.println("I'M UNDER THE WATER!");
-<<<<<<< Updated upstream
         System.out.println("PLEASE HELP ME!");
-=======
-
         System.out.println("HERE TOO MUCH RAINING !!!");
->>>>>>> Stashed changes
     }
 }
