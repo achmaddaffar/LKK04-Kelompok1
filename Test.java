b079638f34f5b7504a36e00c22318361a9221df6
Reimhagen <89968112+Reimhagen@users.noreply.github.com>
2022-07-12 17:07:16 +0700

    Update Test.java
    ada ujan

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HELLO !!!");
         System.out.println("I'M UNDER THE WATER!");
+        System.out.println("PLEASE HELP ME!");
     }
 }
