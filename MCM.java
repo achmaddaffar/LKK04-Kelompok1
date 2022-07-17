0fad5173d04d0b439aefc3a313fdcd937639d4f8
Reimhagen <89968112+Reimhagen@users.noreply.github.com>
2022-07-17 18:36:42 +0700

    Menambah method printOptimalParenthese

--- MCM.java
+++ MCM.java
@@ -4,4 +4,17 @@ public class MCM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     }
+
+    public static void printOptimalParentheses(int i, int j, int n, int[][] p) {
+
+        if (i == j) {
+            System.out.print(x++);
+        } else {
+            System.out.print("(");
+            printOptimalParentheses(i, p[i][j], n, p);
+            printOptimalParentheses(p[i][j] + 1, j, n, p);
+            System.out.print(")");
+        }
+    }
+    
 }
