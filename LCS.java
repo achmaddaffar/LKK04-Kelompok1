57f90344a0460d308589da6df94714cd35b0f8f5
Reqyan <reqyan.1103@gmail.com>
2022-07-17 18:51:25 +0700

    Menambah method LCS_length pada LCS.java

--- LCS.java
+++ LCS.java
@@ -15,6 +15,35 @@ public class LCS {
         Scanner sc = new Scanner(System.in);
     }
 
+    public static return2Var LCS_Length(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
+        String[][] b = new String[m + 1][n + 1];
+        int[][] c = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            c[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            c[0][j] = 0;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    c[i][j] = c[i - 1][j - 1] + 1;
+                    b[i][j] = "\\";
+                } else if (c[i - 1][j] >= c[i][j - 1]) {
+                    c[i][j] = c[i - 1][j];
+                    b[i][j] = "|";
+                } else {
+                    c[i][j] = c[i][j - 1];
+                    b[i][j] = "-";
+                }
+            }
+        }
+        return2Var rtn = new return2Var(c, b);
+        return rtn;
+    }
+
     public static void Print_LCS(String[][] b, String X, int i, int j) {
         if (i == 0 || j == 0)
             return;
@@ -23,8 +52,7 @@ public class LCS {
             System.out.printf("%s", X.substring(i - 1, i));
         } else if (b[i][j].equals("|")) {
             Print_LCS(b, X, i - 1, j);
-        }
-        else {
+        } else {
             Print_LCS(b, X, i, j - 1);
         }
     }
