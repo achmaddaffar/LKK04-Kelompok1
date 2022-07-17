15807f888aa105c3bafb0a0807d750bde1f3d8b8
damieroliverroot <87322409+damieroliverroot@users.noreply.github.com>
2022-07-17 18:53:02 +0700

    Menambah main pada LCS.java

--- LCS.java
+++ LCS.java
@@ -13,6 +13,38 @@ class return2Var {
 public class LCS {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan String X : ");
+        String X = sc.nextLine();
+        System.out.print("Masukkan String Y : ");
+        String Y = sc.nextLine();
+        return2Var rtn = LCS_Length(X, Y);
+        int[][] c = rtn.A;
+        String[][] b = rtn.B;
+        System.out.println("\nTabel LCS-Length :");
+        for (int i = 1; i < c.length; i++) {
+            for (int j = 1; j < c[i].length; j++) {
+                System.out.printf("[%3d] ", c[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int counter1 = 0, counter2 = 0;
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                if (i == 0 & j == 0)
+                    System.out.print("   ");
+                else if (j == 0)
+                    System.out.printf(" %s ", X.substring(counter1, counter1++ + 1));
+                else if (i == 0)
+                    System.out.printf(" %s  ", Y.substring(counter2, counter2++ + 1));
+                else
+                    System.out.printf("[%s] ", b[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.printf("\nLongest Common Subsequence dari %s dan %s adalah ", X, Y);
+        Print_LCS(b, X, X.length(), Y.length());
+        sc.close();
     }
 
     public static return2Var LCS_Length(String X, String Y) {
